ffba94b316140bec012fa94a8bd3205b1c94ed98
Vignesh.Vaidyanathan <vignesh.vaidyanathan@physik.uni-muenchen.de>
2023-10-27 22:40:10 +0200

    added material-ui, react-grid-layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import { Box, Link, Typography } from "@mui/material";
+
+
+const Code = (props : any) => {
+    return (
+        <Typography sx={{ fontFamily : 'Consolas' }} component="span">
+            {props.children}
+        </Typography>
+    )
+}
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Box sx={{ display : 'flex', justifyContent : 'center', flexGrow : 1 }}>
+            <Typography>
+                Use this project as template for <Code>daqpy</Code> based GUI examples at <Link>will be filled</Link>. <br />
+                Install dependencies using <Code>npm install .</Code> or <Code>npm install . --force </Code>
+            </Typography>
+        </Box>
+    );
 }
 
 export default App;
